b58fd3b623b810162915bed2d87929976ea316ab
Rulexloko13 <rubenlechosa@gmail.com>
2022-03-04 18:49:30 +0100

    prueba

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,7 @@ $formcontent = "De: $name \r\nCorreo: $email \r\nAsunto: $subject \r\nMensaje: $
 date_default_timezone_set("Europe/Madrid");
 $formcontent .= "Enviado el: " . date('d/m/Y h:i:sa');
 $recipient = "info@rubenlechosa.com";
-$mailheader = "De: $email \r\n";
+$mailheader = "De: Web Ruben \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error al enviar el mensaje!");
 echo "Mensaje enviado, Gracias!";
 
