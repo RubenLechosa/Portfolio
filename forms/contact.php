36729e6f57e5c35024a698d4ddffc8ce4be8b536
Rulexloko13 <rubenlechosa@gmail.com>
2022-03-04 18:38:35 +0100

    fecha

--- forms/contact.php
+++ forms/contact.php
@@ -5,6 +5,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent = "De: $name \r\nCorreo: $email \r\nAsunto: $subject \r\nMensaje: $message \r\n";
+date_default_timezone_set("Europe/Madrid");
 $formcontent .= "Enviado el: " . date('d/m/Y h:i:sa');
 $recipient = "info@rubenlechosa.com";
 $mailheader = "De: $email \r\n";
